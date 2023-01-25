4c1f738e0237d1fc61aa07b64c1a2e555fa72173
Benoit <benoitded82@hotmail.fr>
2023-01-25 19:17:03 +0100

    Add data of founders and school

---
+++ src/data/dataFounders.jsx
@@ -0,0 +1,70 @@
+export const dataFounders = [
+  {
+    startup: "Lydia",
+    founders: [
+      {
+        name: "Cyril Chiche",
+        photo:
+          "https://pbs.twimg.com/profile_images/917023861538992128/QnHTJlhm_400x400.jpg",
+        role: "CEO",
+        school: "ISG",
+        master: "Master, International Sales and Marketing",
+        graduateYear: "1995",
+        birthdate: "21/09/1998",
+        networks: {
+          linkedin: "https://www.linkedin.com/in/cyrilchiche/",
+          twitter: "https://twitter.com/chichecyril",
+        },
+      },
+      {
+        name: "Antoine Porte",
+        photo:
+          "https://pbs.twimg.com/profile_images/917023861538992128/QnHTJlhm_400x400.jpg",
+        role: "CPO",
+        school: "Panthéon-Assas",
+        master: "M2, Management des NTIC",
+        graduateYear: "2009",
+        birthdate: "21/09/1998",
+        networks: {
+          linkedin: "https://www.linkedin.com/in/antoineporteproduct/",
+          twitter: "https://twitter.com/antoineporte",
+          crunchbase: "https://www.crunchbase.com/person/antoine-porte",
+        },
+      },
+    ],
+  },
+  {
+    startup: "Qonto",
+    founders: [
+      {
+        name: "Alexandre Prot",
+        photo:
+          "https://pbs.twimg.com/profile_images/1481264976476225543/wvJqUg9q_400x400.jpg",
+        role: "CEO",
+        school: "HEC",
+        master: "Master in Finance",
+        graduateYear: "2006",
+        birthdate: "21/09/1998",
+        networks: {
+          linkedin: "https://www.linkedin.com/in/aprot/",
+          twitter: "https://twitter.com/alex_prot",
+          crunchbase: "https://www.crunchbase.com/person/alexandre-prot",
+        },
+      },
+      {
+        name: "Steve Anavi",
+        photo:
+          "https://pbs.twimg.com/profile_images/1481264976476225543/wvJqUg9q_400x400.jpg",
+        role: "President",
+        school: "EPFL",
+        master: "Master of Science (MSc), Microtechnical Engineering",
+        graduateYear: "2006",
+        birthdate: "21/09/1998",
+        networks: {
+          linkedin: "https://www.linkedin.com/in/steveanavi/",
+          crunchbase: "https://www.crunchbase.com/person/steve-anavi",
+        },
+      },
+    ],
+  },
+];
---
+++ src/data/dataSchools.jsx
@@ -0,0 +1,77 @@
+export const dataSchools = [
+  {
+    school: "HEC",
+    fullName: "HEC Paris",
+    logo: "https://pbs.twimg.com/profile_images/935155886636568578/v9G8TXMq_400x400.jpg",
+    banner:
+      "https://pbs.twimg.com/profile_banners/35766159/1656935390/1500x500",
+    ranking: "1",
+    networks: {
+      website: "https://www.hec.edu/fr",
+      linkedin: "https://www.linkedin.com/school/hec-paris/",
+      twitter: "https://twitter.com/HECParis",
+    },
+    country: "France",
+    city: "Paris",
+  },
+  {
+    school: "ESCP",
+    fullName: "ESCP Business School",
+    logo: "https://pbs.twimg.com/profile_images/1503286905496027141/JiA2vZHi_400x400.png",
+    banner:
+      "https://pbs.twimg.com/profile_banners/36327536/1666883513/1500x500",
+    ranking: "2",
+    networks: {
+      website: "https://escp.eu/",
+      linkedin: "https://www.linkedin.com/school/escp-business-school/",
+      twitter: "https://twitter.com/ESCP_bs",
+    },
+    country: "France",
+    city: "Paris",
+  },
+  {
+    school: "ISG",
+    fullName: "ISG Institut Supérieur de Gestion",
+    logo: "https://pbs.twimg.com/profile_images/1587744373420294147/Y1eXKMhH_400x400.jpg",
+    banner: "https://pbs.twimg.com/profile_banners/8054872/1665649969/1500x500",
+    ranking: "5",
+    networks: {
+      website: "https://www.isg.fr/",
+      linkedin:
+        "https://www.linkedin.com/school/institut-sup%C3%A9rieur-de-gestion/",
+      twitter: "https://twitter.com/ISG",
+    },
+    country: "France",
+    city: "Paris",
+  },
+  {
+    school: "Panthéon-Assas",
+    fullName: "Panthéon-Assas université",
+    logo: "https://pbs.twimg.com/profile_images/1481177442370957313/aClLuraT_400x400.jpg",
+    banner:
+      "https://pbs.twimg.com/profile_banners/303849254/1663838623/1500x500",
+    ranking: "",
+    networks: {
+      website: "http://www.assas-universite.fr",
+      linkedin: "https://www.linkedin.com/school/assasuniversite/",
+      twitter: "https://twitter.com/AssasUniversite",
+    },
+    country: "France",
+    city: "Paris",
+  },
+  {
+    school: "EPFL",
+    fullName: "Panthéon-Assas université",
+    logo: "https://pbs.twimg.com/profile_images/1107673782133637121/HDPlE4jz_400x400.png",
+    banner:
+      "https://pbs.twimg.com/profile_banners/108364911/1668431787/1500x500",
+    ranking: "",
+    networks: {
+      website: "https://www.epfl.ch/en/",
+      linkedin: "https://www.linkedin.com/school/epfl/",
+      twitter: "https://twitter.com/EPFL",
+    },
+    country: "Suisse",
+    city: "Lausanne",
+  },
+];
